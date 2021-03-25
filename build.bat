08cdc9c35f7b9c7698bf21f09c3369e71d2314d8
Лёня Декарт <lyonya.decart@gmail.com>
2021-03-25 12:58:17 +0500

    mini_app

--- build.bat
+++ build.bat
@@ -3,6 +3,6 @@ git add .
 git commit -m "mini_app"
 git remote add origin https://github.com/LeonidDecart/firstMini.git
 git push origin master
-npm install --save gh-pages
 npm run deploy
+npm start
 pause
